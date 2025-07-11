360e39c62976f1e213d07144565d5969c8ff4d00
christina ives <christinaives@christinas-Air.attlocal.net>
2025-07-11 00:09:45 -0700

    added event tracking request function

--- iterable.py
+++ iterable.py
@@ -4,26 +4,25 @@ from dotenv import load_dotenv
 from datetime import datetime
 from customer_data import fetch_data
 
-# Load environment variables from .env file
-load_dotenv()
+def track_users(payload):
+    event_type = {"eventName": "page_view"}
+    payload.update(event_type)
+    try:
+        response = requests.post(iterable_event_endpoint, headers=headers, json=payload)
+        # Raise an HTTPError for bad responses (4xx or 5xx)
+        response.raise_for_status()
+        print("Response status code:", response.status_code)
+        print("Response JSON:", response.json())
+        print("User event has been tracked")
 
-# Access the API key
-api_key = os.getenv("API_KEY")
-iterable_user_endpoint = "https://api.iterable.com/api/users/update"
-iterable_event_endpoint = "https://api.iterable.com/api/events/track"
-
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Content-Type": "application/json"
-}
-
-# headers = {
-#     "X-API-Key": api_key,
-#     "Content-Type": "application/json"
-# }
-
-def track_users():
-    print('hi')
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
+    except requests.exceptions.ConnectionError as e:
+        print(f"Connection error: {e}")
+    except requests.exceptions.Timeout as e:
+        print(f"Request timed out: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"An unexpected error occurred: {e}")
 
 def create_users(payload):
     try:
@@ -32,20 +31,24 @@ def create_users(payload):
         response.raise_for_status()
         print("Response status code:", response.status_code)
         print("Response JSON:", response.json())
+        print("Users have been created or updated")
 
+        track_users(payload)
+
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
     except requests.exceptions.ConnectionError as e:
         print(f"Connection error: {e}")
     except requests.exceptions.Timeout as e:
         print(f"Request timed out: {e}")
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error: {e}")
     except requests.exceptions.RequestException as e:
         print(f"An unexpected error occurred: {e}")
 
 def format_user_data():
+    # fetch_data is imported from customer_data.py
     customers = fetch_data()
     for row in customers:
-        # cast int to string for iterable api
+        # cast int to string for api
         user_id = str(row[0])
         first_name = row[1]
         last_name = row[2]
@@ -60,7 +63,6 @@ def format_user_data():
         candidate = row[10]
 
         payload = {
-            "createNewFields": True,
             "dataFields": {
                 "first_name": first_name,
                 "last_name": last_name,
@@ -73,13 +75,27 @@ def format_user_data():
                 "event_time": event_time,
                 "candidate": candidate
             },
-            "email": email,
-            "mergeNestedObjects": True,
-            "preferUserId": True,
-            "userId": user_id
+            "email": email
         }
         create_users(payload)
 
 
 if __name__ == "__main__":
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Access the API key
+    api_key = os.getenv("API_KEY")
+    iterable_user_endpoint = "https://api.iterable.com/api/users/update"
+    iterable_event_endpoint = "https://api.iterable.com/api/events/track"
+
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+
+    # headers = {
+    #     "X-API-Key": api_key,
+    #     "Content-Type": "application/json"
+    # }
     format_user_data()
